37a20c826111e75df383c100e700c11ef6237d66
mycah4 <116039153+mycah4@users.noreply.github.com>
2026-05-08 18:10:39 -0400

    Update game.py

--- game.py
+++ game.py
@@ -57,7 +57,7 @@ def create_player():
             elif user_choice == 2:
                 player_class = "Engineer"
         
-            elif:
+            elif
                 player_class = "Medic"
 
             else:
