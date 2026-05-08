2398595e063f023be49727d28eb431f9586f95a0
mycah4 <116039153+mycah4@users.noreply.github.com>
2026-05-08 15:04:44 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
+while True:
+  print("\n Main Menu")      # main menu prompt 
+  print("1. Start Game")
+  print("2. Exit game:")
 
+  user_choice = input("Choose: ")
+
+  if choice == "1":
+    print("Game starting")         # allows user to either leave or start game
+    break
+  elif choice = "2":
+    print("See you next time!")
+    break
+  else:
+    print("You didn't choose")
+
+story_level = 1     # global variables
+health = 100
+inventory = []
+player_name = ""
+player_class ""
