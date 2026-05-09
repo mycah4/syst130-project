407d2d3d753d43c4a4532546ed7808b4242e79ba
mycah4 <116039153+mycah4@users.noreply.github.com>
2026-05-09 05:12:16 -0400

    Update game.py

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ player_class = ""
 
 # ____________________________________________________________________    # AUDIT LOGGING
 
-def create_audit_log(event_type, details):
+def create_audit_log(event_type, details, timestamp):
     timestamp = datatime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 with open("audit_log.txt", "a") as log_file:
