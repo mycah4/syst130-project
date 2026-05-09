fcd7550485ff6b51a0beb85856ff9e6f91965d15
mycah4 <116039153+mycah4@users.noreply.github.com>
2026-05-09 05:24:44 -0400

    Update audit.py

--- audit.py
+++ audit.py
@@ -1,1 +1,8 @@
+from datetime
+import datetime
 
+def create_audit_log(event_type, details, timestamp):
+    timestamp = datatime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open("audit_log.txt", "a") as log_file:
+        log_file.write(f"[{timestamp}] {event_type}: {details}\n")
